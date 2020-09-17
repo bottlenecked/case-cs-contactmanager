5db83449b79a0cd6c27d9cecadf227c7926d1852
Manos Emmanouilidis <m.emmanouilidis@stoiximan.gr>
2020-09-17 23:16:57 +0300

    fix(test) fix broken test from prev commit

--- test/case_cs_contact_manager_web/controllers/page_controller_test.exs
+++ test/case_cs_contact_manager_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule CaseCsContactManagerWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "List of cases with active contacts"
   end
 end
