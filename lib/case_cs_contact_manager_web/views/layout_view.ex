9720a45f96e6fbec244ec7ede2357d4ceea6f2b3
Manos Emmanouilidis <m.emmanouilidis@stoiximan.gr>
2020-09-20 17:39:59 +0300

    feat(app) stylistic changes. This closes #7

--- lib/case_cs_contact_manager_web/views/layout_view.ex
+++ lib/case_cs_contact_manager_web/views/layout_view.ex
@@ -1,3 +1,8 @@
 defmodule CaseCsContactManagerWeb.LayoutView do
   use CaseCsContactManagerWeb, :view
+  alias CaseCsContactManager.Accounts
+
+  def signed_in?(conn) do
+    Accounts.current_user_id(conn) != nil
+  end
 end
